e2e18a2563a9a5746bcc4fbac431fd4c3da04087
Evgeny <mishkin-583@mail.ru>
2022-12-04 17:34:35 +0300

    html commit

---
+++ second commit/91.js
@@ -0,0 +1,13 @@
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum < startNum) {
+      return [];
+    } else {
+    const number = rangeOfNumbers(startNum, endNum - 1);
+    number.push(endNum);
+     return number; 
+    }
+    
+  };
+  
+  
+  
